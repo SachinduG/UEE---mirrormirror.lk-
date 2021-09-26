306475c6d55a6b297589d39319a1e21d172d010c
Gamitha <gamithamanawadu26@gmail.com>
2021-09-26 12:31:07 +0530

    profile added

--- app/src/main/java/com/example/mirror/Store.java
+++ app/src/main/java/com/example/mirror/Store.java
@@ -7,13 +7,20 @@ import android.os.Bundle;
 import android.widget.ImageView;
 
 public class Store extends AppCompatActivity {
-    private ImageView women;
+    private ImageView women, profile;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store);
 
         women = findViewById(R.id.imageView12);
+        profile = findViewById(R.id.imageprofile);
+
+        profile.setOnClickListener(view -> {
+            Intent send = new Intent(Store.this, Profile.class);
+            startActivity(send);
+        });
 
         women.setOnClickListener(view -> {
             Intent send = new Intent(Store.this, WomenItemList.class);
