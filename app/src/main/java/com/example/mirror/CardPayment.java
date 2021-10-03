47cd1d5ddc968fd3dfa3701d5a440f7076178c0b
Sachindu <sachindugimhana0@gmail.com>
2021-10-03 18:02:09 +0530

    card form created

--- app/src/main/java/com/example/mirror/CardPayment.java
+++ app/src/main/java/com/example/mirror/CardPayment.java
@@ -2,7 +2,10 @@ package com.example.mirror;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.Objects;
 
@@ -14,5 +17,14 @@ public class CardPayment extends AppCompatActivity {
         setContentView(R.layout.activity_card_payment);
         setTitle("Card Payment");
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        Button button = findViewById(R.id.btn_confirm);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(CardPayment.this, CardForm.class));
+            }
+        });
     }
 }
