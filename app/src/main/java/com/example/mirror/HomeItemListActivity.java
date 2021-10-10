d74b371f3f3cfb291913d23bd754154a8d81be5d
Sanduni-Gamage <sandunijgamage@gmail.com>
2021-10-10 12:11:21 +0530

    home item list

---
+++ app/src/main/java/com/example/mirror/HomeItemListActivity.java
@@ -0,0 +1,14 @@
+package com.example.mirror;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeItemListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_item_list);
+    }
+}
